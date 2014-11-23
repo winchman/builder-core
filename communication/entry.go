96bd0043dde8870eafee759d808fdb5d67fb108e
Rafe Colton <r.colton@modcloth.com>
2014-11-23 10:29:53 -0800

    Adding comments, commenting out some things that are not finished

--- communication/entry.go
+++ communication/entry.go
@@ -6,26 +6,33 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
-type State uint8
+//type State uint8
 
-const (
-	Requested State = iota
-	Building
-	Pushing
-	Completed
-	Errored
-)
+//const (
+//Requested State = iota
+//Building
+//Pushing
+//Completed
+//Errored
+//)
 
 type (
-	LogChan    chan LogEntry
+	// LogChan is a channel for log entries
+	LogChan chan LogEntry
+
+	// StatusChan is a channel for status updates (somewhat TBD)
 	StatusChan chan StatusEntry
-	ExitChan   chan error
+
+	// ExitChan is a channel for receiving the final exit value (error or nil)
+	ExitChan chan error
 )
 
+// LogEntry is a convenient, extensible way to ship logrus log entries
 type LogEntry interface {
 	Entry() *logrus.Entry
 }
 
+// NewLogEntry produces a log entry that can be sent on a LogChan
 func NewLogEntry(l *logrus.Entry) LogEntry {
 	return (*logEntry)(l)
 }
@@ -36,26 +43,27 @@ func (l *logEntry) Entry() *logrus.Entry {
 	return (*logrus.Entry)(l)
 }
 
-// StatusMessage is the tentative interface for the structs returned on the status channel
+// StatusEntry is the tentative interface for the structs returned on the status channel
 type StatusEntry interface {
 	BuildID() int
-	PreviousState() State
-	CurrentState() State
+	//PreviousState() State
+	//CurrentState() State
 	Note() string
 	//Error() error // should be checked for non-nil
 }
 
 // NewLogEntryWriter returns a log entry writer initialized with the provided
 // channel. The provided writer implements the io.Writer interface
-func NewLogEntryWriter(ch LogChan) *logEntryWriter {
-	return &logEntryWriter{log: ch}
+func NewLogEntryWriter(ch LogChan) *LogEntryWriter {
+	return &LogEntryWriter{log: ch}
 }
 
-type logEntryWriter struct {
+// LogEntryWriter is a type for implementing the io.Writer interface
+type LogEntryWriter struct {
 	log LogChan
 }
 
-func (writer *logEntryWriter) Write(p []byte) (n int, err error) {
+func (writer *LogEntryWriter) Write(p []byte) (n int, err error) {
 	lines := strings.Split(string(p), "\n")
 	for _, line := range lines {
 		writer.log <- (*logEntry)(&logrus.Entry{Message: string(line)})
