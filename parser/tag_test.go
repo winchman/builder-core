2cefa1872a94aefbafb61ce2730833ef74d18c93
Rafe Colton <r.colton@modcloth.com>
2014-12-07 05:16:46 +0100

    Adding tests for date-based tags

--- parser/tag_test.go
+++ parser/tag_test.go
@@ -9,12 +9,12 @@ import (
 )
 
 func TestTemplateBasedTags(t *testing.T) {
-	var input = ` latest {{ branch }} {{ sha }} {{ tag }} `
+	var input = ` latest {{ branch }} {{ sha }} {{ tag }} daily-{{ date "2006-01-02" }} `
 	var tag = NewTag(input)
 
 	var actual = tag.Evaluate(os.Getenv("PWD"))
 	top := os.Getenv("PWD")
-	expected := fmt.Sprintf(` latest %s %s %s `, git.Branch(top), git.Sha(top), git.Tag(top))
+	expected := fmt.Sprintf(` latest %s %s %s daily-%s `, git.Branch(top), git.Sha(top), git.Tag(top), os.Getenv("DATE"))
 	if expected != actual {
 		t.Errorf("expected %q, got %q", expected, actual)
 	}
